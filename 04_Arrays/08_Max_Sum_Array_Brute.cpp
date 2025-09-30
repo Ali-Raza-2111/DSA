a95f31a71fac6f99f77c7cd0001a67226314183f
Ali Raza <aliraza21112005@gmail.com>
2025-09-29 20:24:17 -0700

    Max Sub Array

---
+++ 04_Arrays/08_Max_Sum_Array_Brute.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void maxSubArraySum(int *arr,int n){
+    int maxSum = INT_MIN;
+    for (int start = 0; start < n; start++)
+    {
+        for (int end = start; end < n; end++)
+        {
+            int currSum=0;
+            
+            for (int i = start; i <=end; i++)
+            {
+                currSum +=arr[i];
+            }
+            maxSum = max(maxSum,currSum);
+           
+        }
+    }
+    cout<<"Maximum Sum is "<<maxSum<<endl;
+}
+int main(){
+    int arr[6] = {2,-3,6,-5,4,2};
+    int n = sizeof(arr)/sizeof(int);
+
+    maxSubArraySum(arr,n);
+
+    return 0;
+}
---
+++ 04_Arrays/09_Max_Sum_Array_opt.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+void maxSubArraySum(int *arr,int n){
+    int maxSum = INT_MIN;
+    for (int start = 0; start < n; start++)
+    {
+        int currSum=0;
+        for (int end = start; end < n; end++)
+        {
+            currSum+=arr[end];
+            
+            maxSum = max(maxSum,currSum);
+           
+        }
+    }
+    cout<<"Maximum Sum is "<<maxSum<<endl;
+}
+
+
+int main(){
+     int arr[6] = {2,-3,6,-5,4,2};
+    int n = sizeof(arr)/sizeof(int);
+
+    maxSubArraySum(arr,n);
+    return 0;
+}
