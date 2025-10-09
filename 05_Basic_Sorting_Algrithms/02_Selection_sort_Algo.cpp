d3451a7923ceb99d9d3a86ae00d1d06826ddf8d4
Ali Raza <aliraza21112005@gmail.com>
2025-10-08 23:28:22 -0700

    Selection Sort Completed

---
+++ 05_Basic_Sorting_Algrithms/02_Selection_sort_Algo.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void printArr(int arr[],int n){
+    for (int i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    cout<<endl;
+}
+void selectionSort(int arr[],int n){
+    for (int i = 0; i < n-1; i++)
+    {
+        int minIndx = i;
+        for (int j = i+1; j < n; j++)
+        {
+            if (arr[j]< arr[minIndx])
+            {
+               minIndx = j;
+            }
+            
+        }
+        swap(arr[i],arr[minIndx]);
+        
+    }
+    printArr(arr,n);
+}
+int main(){
+    int arr[5] = {5,4,3,1,2};
+    int n = sizeof(arr)/sizeof(int);
+    selectionSort(arr,n);
+    return 0;
+}
