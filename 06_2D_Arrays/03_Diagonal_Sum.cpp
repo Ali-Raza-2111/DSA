9a38dff22342f56ed400c61df4fca2559eebd0ae
Ali Raza <aliraza21112005@gmail.com>
2025-10-19 10:03:19 -0700

    Diagonal Matrix

---
+++ 06_2D_Arrays/03_Diagonal_Sum.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+void diagonalSum(int matrix[][4],int n){
+    int sum = 0;
+    for(int i = 0;i<n;i++){
+        sum+=matrix[i][i];
+        if(i!= n-i-1){
+            sum+=matrix[i][n-i-1];
+        }
+    }
+    cout<<"Sum is "<<sum;
+}
+int main(){
+    int matrix[4][4] = {{1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}};
+    
+
+    diagonalSum(matrix,4);
+    return 0;
+}
