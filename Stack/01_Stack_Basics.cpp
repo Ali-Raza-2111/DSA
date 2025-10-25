346b1690313253c87ae0a9cbef1e149df8d86280
Ali Raza <aliraza21112005@gmail.com>
2025-10-25 06:23:32 -0700

    Stack Started

---
+++ Stack/01_Stack_Basics.cpp
@@ -0,0 +1,81 @@
+#include<iostream>
+using namespace std;
+
+const int STACKSIZE = 5;
+class stack{
+    private:
+    int stackArray[STACKSIZE];
+    int TOP;
+
+    public:
+    stack(){
+        TOP = -1;
+    }
+
+    bool isEmpty(){
+        if(TOP == -1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    bool isFull(){
+        if (TOP == STACKSIZE - 1)
+        {
+            return true;
+        }else{
+            return false;
+        }
+        
+    }
+
+    void Push(int element){
+        if (isFull() == true)
+        {
+            cout<<"Can't insert Element because the stack is already Full";
+        }else{
+            TOP++;
+            stackArray[TOP] = element;
+            
+        }
+    }
+
+    void Pop(){
+        if (isEmpty() == true)
+        {
+            cout<<"Cannot remove the element Because the stack is already Empty";
+        }else{
+            TOP--;
+        }
+        
+    }
+    int size(){
+        return TOP +1;
+    }
+    void DisplayStack(){
+        if (TOP == 0)
+        {
+            cout<<"No element in the Stack";
+            return;
+        }else{
+            for (int i = size()-1; i >=0 ; i--)
+            {
+                cout<<stackArray[i]<<endl;
+            }
+            
+        }
+        
+    }
+};
+int main(){
+    stack st;
+    st.Push(4);
+    st.Push(5);
+    st.Push(6);
+    st.DisplayStack();
+    st.Pop();
+    cout<<endl;
+    cout<<endl;
+    st.DisplayStack();
+}
